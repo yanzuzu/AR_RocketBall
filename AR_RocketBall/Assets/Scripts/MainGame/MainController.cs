b7156f64d81b46a54ebed22071f7beb09e96dd4f
Sam Lo <sam@randomidea.com>
2018-06-21 22:03:59 +0800

    simple car controller.

---
+++ AR_RocketBall/Assets/Scripts/MainGame/MainController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour 
+{
+	private IVehicleCtrl m_ctrl;
+	private Vector2 m_steer;
+
+	public void SetVehicle( IVehicleCtrl vehicle )
+	{
+		m_ctrl = vehicle;
+	}
+
+	public void OnEnterRight()
+	{
+		m_steer.x = 1;
+		m_ctrl.SetSteering (m_steer);
+	}
+
+	public void OnLeaveRight()
+	{
+		m_steer.x = 0;
+		m_ctrl.SetSteering (m_steer);
+	}
+
+	public void OnEnterLeft()
+	{
+		m_steer.x = -1;
+		m_ctrl.SetSteering (m_steer);
+	}
+
+	public void OnLeaveLeft()
+	{
+		m_steer.x = 0;
+		m_ctrl.SetSteering (m_steer);
+	}
+
+	public void OnEnterGas()
+	{
+		m_ctrl.SetForwardOn ();
+	}
+
+	public void OnLeaveGas()
+	{
+		m_ctrl.SetForwardOff ();
+	}
+
+	public void OnEnterBack()
+	{
+		m_ctrl.SetBackwardOn ();
+	}
+
+	public void OnLeaveBack()
+	{
+		m_ctrl.SetBackwardOff ();
+	}
+}
+
+
---
+++ AR_RocketBall/Assets/Scripts/MainGame/MainGameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class MainGameManager : MonoBehaviour 
+{
+	[SerializeField]
+	private MainController m_controller;
+
+	public const float CreateHeight = 0.3f;
+	private const string VEHICLE_RES_PATH = "Vehicles/Car_1";
+
+	private Vehicle m_vehicle;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_controller.gameObject.SetActive (false);
+	}
+
+	void CreateCar(Vector3 atPosition)
+	{
+		GameObject carObj = Instantiate (Resources.Load(VEHICLE_RES_PATH), atPosition, Quaternion.identity) as GameObject;
+		m_vehicle = carObj.GetComponent<Vehicle> ();
+		VehiclePlayerCtrl playCtrl = carObj.AddComponent<VehiclePlayerCtrl> ();
+
+		m_controller.gameObject.SetActive (true);
+		m_controller.SetVehicle (playCtrl);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (m_vehicle != null)
+		{
+			return;
+		}
+
+		if (Input.touchCount <= 0)
+		{
+			return;
+		}
+
+		var touch = Input.GetTouch(0);
+		if (touch.phase == TouchPhase.Began)
+		{
+			var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+			ARPoint point = new ARPoint {
+				x = screenPosition.x,
+				y = screenPosition.y
+			};
+
+			List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface ().HitTest (point, 
+				ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent);
+			if (hitResults.Count > 0) 
+			{
+				foreach (ARHitTestResult hitResult in hitResults) 
+				{
+					Vector3 position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+					CreateCar (new Vector3 (position.x, position.y + CreateHeight, position.z));
+					break;
+				}
+			}
+
+		}
+
+	}
+}
